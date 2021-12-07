93e2a0db6c7efc24d0b268a1d5a7749711573063
Ross Acheson <rossacheson@users.noreply.github.com>
2021-12-07 11:36:11 -0500

    Add BooksScreen and stack navigation

---
+++ src/screens/BooksScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const BooksScreen = (props) => {
+  return (
+    <View>
+      <Text>BooksScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default BooksScreen;
